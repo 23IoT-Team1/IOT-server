5ad0efa7ab2a989e4d3b7a338fa0a371a09e77c7
lib26 <dladlsqja@naver.com>
2023-05-16 23:30:43 +0900

    feat: init commit

---
+++ src/main/java/iot/wifi_map/WifiMapApplication.java
@@ -0,0 +1,13 @@
+package iot.wifi_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iot/wifi_map/WifiMapApplicationTests.java
@@ -0,0 +1,13 @@
+package iot.wifi_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
